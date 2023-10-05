20a1187adf57c915597175818591c7bc68af2fe9
Igor Grégoire <54171035+igivotron@users.noreply.github.com>
2023-10-05 16:04:40 +0200

    Update Devoir1.py

--- Devoir1/Devoir1.py
+++ Devoir1/Devoir1.py
@@ -1,5 +1,31 @@
-def devoir1(trajets_train, durees_train, ville_depart, durees_voldirect, sacrifice):
 
-    # TO DO
+trajets = ["S-K", "S-J", "K-J", "K-M", "J-L", "M-L", "L-N"]
+poids = [2, 4, 5, 3, 10, 4, 6]
 
-    return ville, min_distances_train, min_distances_avion, count_train
+
+def extract_cities(t):
+    cities = []
+    for i in t:
+        for j in i.split("-"):
+            if j not in cities:
+                cities.append(j)
+    return sorted(cities)
+
+def distance(t, p, start, stop):
+    l = []
+    for i in range(len(t)):
+        if start in t[i] and stop in t[i]:
+            return p[i]
+    return None
+
+def dijkstra(trajets, poids, start):
+    N = extract_cities(trajets)
+    Ns = [start]
+    N.remove(start)
+    m = []
+    n = []
+    i=0
+    for i in range(len(N)):
+        n.append(distance(trajets, poids, start, N[i]))
+
+
