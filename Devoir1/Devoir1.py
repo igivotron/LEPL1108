c74f9beab9a6f487fafde6fbd3255f09840a6892
Igiv Igiv <gregoire.igor@gmail.com>
2023-10-09 19:54:26 +0200

    There is an error in dijkstra

--- Devoir1/Devoir1.py
+++ Devoir1/Devoir1.py
@@ -1,5 +1,19 @@
-trajets = ["S-K", "S-J", "K-J", "K-M", "J-L", "M-L", "L-N"]
-poids = [2, 4, 5, 3, 10, 4, 6]
+trajets_train = ["BRU-PAR", "BRU-AMS", "BRU-LON", "BRU-COL", "PAR-BOR", 
+                "PAR-LYO", "PAR-FRA", "PAR-LON", "PAR-REN", "AMS-BER", 
+                "AMS-COL", "AMS-HAM", "COL-HAM", "COL-FRA", "COL-BER", 
+                "LYO-MAR", "LYO-ZUR", "FRA-HAM", "FRA-BER", "FRA-MUN", 
+                "FRA-ZUR", "BER-MUN", "BER-HAM", "BER-PRA", "MUN-ZUR", 
+                "MIL-LYO", "MIL-ZUR", "LYO-BAR", "BAR-MAR", "BOR-TLS", 
+                "TLS-BAR", "TLS-MAR", "LON-BIR", "LON-MAN", "MAN-BIR", 
+                "MAN-EDI", "EDI-GLW", "LYO-TLS", "HAM-COP", "PAR-TLS"]
+durees_train = [80, 95, 120, 105, 120, 110, 230, 140, 90, 385, 180, 
+                300, 215, 60, 280, 110, 240, 280, 275, 200, 235, 255, 
+                175, 265, 210, 270, 240, 330, 285, 120, 195, 225, 95, 
+                140, 80, 190, 50, 250, 280, 260]
+ville_depart = "BRU"
+durees_voldirect = [65, 120, 85, 80, 100, 60, 90, 100, 60, 105, 75, 80, 85, 
+                85, 100, 90, 80, 65, 90, 95, 100, 75]
+sacrifice = 60
 
 
 def extract_cities(t):
@@ -24,24 +38,18 @@ def dijkstra(t, p, start):
     N = extract_cities(t)
     N.remove(start)
     d= {}
-    for i in N:
-        d[i] = [distance(t, p, start, i), 1] # liste contenant la distance et le nombre de transfert
+    for i in N: # Première boucle, initialisation du dictionnaire
+        d[i] = [distance(t, p, start, i), 0]
     
     start = min(d, key=d.get)
     N.remove(start)
 
     while N != []:
         for i in N:
-            diPrime = distance(t, p, start, i) + d[start][0]
-            if diPrime < d[i][0]:
-                d[i][0] = diPrime
-                d[i][1] = d[start][1] +1     # Non fonctionnel car se décale
-
-            #d[i] = min(d[i], distance(t, p, start, i) + d[start])
-        start = min(N ,key=lambda x : d[x][0])
+            d[i] = min(d[i], distance(t, p, start, i) + d[start])
+        start = min(N ,key=lambda x : d[x]) # On cherche le minimum dans la liste N à partir du dictionnaire
         N.remove(start)
 
-    return d
+    return d # retourne un dictionnaire
 
-d = dijkstra(trajets, poids, "M")
-print(d)
+print(extract_cities(trajets_train))
--- Devoir1/Devoir1_class.py
+++ Devoir1/Devoir1_class.py
@@ -1,3 +1,23 @@
+trajets_train = ["BRU-PAR", "BRU-AMS", "BRU-LON", "BRU-COL", "PAR-BOR", 
+                "PAR-LYO", "PAR-FRA", "PAR-LON", "PAR-REN", "AMS-BER", 
+                "AMS-COL", "AMS-HAM", "COL-HAM", "COL-FRA", "COL-BER", 
+                "LYO-MAR", "LYO-ZUR", "FRA-HAM", "FRA-BER", "FRA-MUN", 
+                "FRA-ZUR", "BER-MUN", "BER-HAM", "BER-PRA", "MUN-ZUR", 
+                "MIL-LYO", "MIL-ZUR", "LYO-BAR", "BAR-MAR", "BOR-TLS", 
+                "TLS-BAR", "TLS-MAR", "LON-BIR", "LON-MAN", "MAN-BIR", 
+                "MAN-EDI", "EDI-GLW", "LYO-TLS", "HAM-COP", "PAR-TLS"]
+durees_train = [80, 95, 120, 105, 120, 110, 230, 140, 90, 385, 180, 
+                300, 215, 60, 280, 110, 240, 280, 275, 200, 235, 255, 
+                175, 265, 210, 270, 240, 330, 285, 120, 195, 225, 95, 
+                140, 80, 190, 50, 250, 280, 260]
+ville_depart = "BRU"
+durees_voldirect = [65, 120, 85, 80, 100, 60, 90, 100, 60, 105, 75, 80, 85, 
+                85, 100, 90, 80, 65, 90, 95, 100, 75]
+
+t = ["S-K", "S-J", "K-J", "K-M", "J-L", "M-L", "L-N"]
+p = [2, 4, 5, 3, 10, 4, 6]
+
+sacrifice = 60
 
 class Mappy:
     def __init__(self, trajets, poids):
@@ -25,20 +45,44 @@ class Mappy:
         N.remove(start)
         d= {}
         for i in N:
-            d[i] = self.distance(start, i)
+            d[i] = [self.distance(start, i),0]
         
         start = min(d, key=d.get)
         N.remove(start)
 
         while N != []:
             for i in N:
-                diPrime = self.distance(self.t, self.p, start, i) + d[start][0]
+                diPrime = self.distance(start, i) + d[start][0]
                 if diPrime < d[i][0]:
                     d[i][0] = diPrime
-                    d[i][1] = d[start][1] +1    
-                
-            start = min(N ,key=lambda x : d[x][0])
+                    d[i][1] = d[start][1] +1
+
+            start = min(N, key=lambda x : d[x][0])
             N.remove(start)
 
         return d
-    
+
+carte = Mappy(trajets_train, durees_train)
+train = carte.dijkstra(ville_depart)
+
+
+
+ville = list(train.keys())
+min_distances_train = []
+min_distances_avion = []
+for i in train:
+    print(i,train[i])
+    min_distances_train.append(train[i][0] + 15*train[i][1] + 30)
+
+for i in durees_voldirect:
+    min_distances_avion.append(i+180)
+
+count_train = 0
+for i in range(len(min_distances_train)):
+    if min_distances_avion[i] + sacrifice > min_distances_train[i]:
+        count_train +=1
+
+
+
+
+
